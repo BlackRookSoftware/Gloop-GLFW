e72a07f2ee2f8fd22ebb5960bbca18cc392dc3ca
mtrop <mtropiano@gmail.com>
2020-05-05 01:03:31 -0400

    Add stuff

--- src/main/java/com/blackrook/gloop/glfw/GLFWWindow.java
+++ src/main/java/com/blackrook/gloop/glfw/GLFWWindow.java
@@ -15,32 +15,7 @@ public class GLFWWindow extends GLFWHandle
 	private boolean allocated;
 
 	/**
-	 * Window hints.
-     * <table class=striped>
-     * <tr><th>Name</th><th>Default value</th><th>Supported values</th></tr>
-     * <tr><td>GLFW_ACCUM_RED_BITS ACCUM_RED_BITS</td><td>0</td><td>0 to MAX_VALUE or GLFW_DONT_CARE DONT_CARE</td></tr>
-     * <tr><td>GLFW_ACCUM_GREEN_BITS ACCUM_GREEN_BITS</td><td>0</td><td>0 to MAX_VALUE or GLFW_DONT_CARE DONT_CARE</td></tr>
-     * <tr><td>GLFW_ACCUM_BLUE_BITS ACCUM_BLUE_BITS</td><td>0</td><td>0 to MAX_VALUE or GLFW_DONT_CARE DONT_CARE</td></tr>
-     * <tr><td>GLFW_ACCUM_ALPHA_BITS ACCUM_ALPHA_BITS</td><td>0</td><td>0 to MAX_VALUE or GLFW_DONT_CARE DONT_CARE</td></tr>
-     * <tr><td>GLFW_AUX_BUFFERS AUX_BUFFERS</td><td>0</td><td>0 to MAX_VALUE</td></tr>
-     * <tr><td>GLFW_SAMPLES SAMPLES</td><td>0</td><td>0 to MAX_VALUE</td></tr>
-     * <tr><td>GLFW_REFRESH_RATE REFRESH_RATE</td><td>GLFW_DONT_CARE DONT_CARE</td><td>0 to MAX_VALUE or GLFW_DONT_CARE DONT_CARE</td></tr>
-     * <tr><td>GLFW_STEREO STEREO</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_SRGB_CAPABLE SRGB_CAPABLE</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_DOUBLEBUFFER DOUBLEBUFFER</td><td>GLFW_TRUE TRUE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_CLIENT_API CLIENT_API</td><td>GLFW_OPENGL_API OPENGL_API</td><td>GLFW_NO_API NO_API GLFW_OPENGL_API OPENGL_API GLFW_OPENGL_ES_API OPENGL_ES_API</td></tr>
-     * <tr><td>GLFW_CONTEXT_CREATION_API CONTEXT_CREATION_API</td><td>GLFW_NATIVE_CONTEXT_API NATIVE_CONTEXT_API</td><td>GLFW_NATIVE_CONTEXT_API NATIVE_CONTEXT_API GLFW_EGL_CONTEXT_API EGL_CONTEXT_API GLFW_OSMESA_CONTEXT_API OSMESA_CONTEXT_API</td></tr>
-     * <tr><td>GLFW_CONTEXT_VERSION_MAJOR CONTEXT_VERSION_MAJOR</td><td>1</td><td>Any valid major version number of the chosen client API</td></tr>
-     * <tr><td>GLFW_CONTEXT_VERSION_MINOR CONTEXT_VERSION_MINOR</td><td>0</td><td>Any valid minor version number of the chosen client API</td></tr>
-     * <tr><td>GLFW_CONTEXT_ROBUSTNESS CONTEXT_ROBUSTNESS</td><td>GLFW_NO_ROBUSTNESS NO_ROBUSTNESS</td><td>GLFW_NO_ROBUSTNESS NO_ROBUSTNESS GLFW_NO_RESET_NOTIFICATION NO_RESET_NOTIFICATION GLFW_LOSE_CONTEXT_ON_RESET LOSE_CONTEXT_ON_RESET</td></tr>
-     * <tr><td>GLFW_CONTEXT_RELEASE_BEHAVIOR CONTEXT_RELEASE_BEHAVIOR</td><td>GLFW_ANY_RELEASE_BEHAVIOR ANY_RELEASE_BEHAVIOR</td><td>GLFW_ANY_RELEASE_BEHAVIOR ANY_RELEASE_BEHAVIOR GLFW_RELEASE_BEHAVIOR_FLUSH RELEASE_BEHAVIOR_FLUSH GLFW_RELEASE_BEHAVIOR_NONE RELEASE_BEHAVIOR_NONE</td></tr>
-     * <tr><td>GLFW_CONTEXT_NO_ERROR CONTEXT_NO_ERROR</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_OPENGL_FORWARD_COMPAT OPENGL_FORWARD_COMPAT</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_OPENGL_DEBUG_CONTEXT OPENGL_DEBUG_CONTEXT</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_OPENGL_PROFILE OPENGL_PROFILE</td><td>GLFW_OPENGL_ANY_PROFILE OPENGL_ANY_PROFILE</td><td>GLFW_OPENGL_ANY_PROFILE OPENGL_ANY_PROFILE GLFW_OPENGL_CORE_PROFILE OPENGL_CORE_PROFILE GLFW_OPENGL_COMPAT_PROFILE OPENGL_COMPAT_PROFILE</td></tr>
-     * <tr><td>GLFW_COCOA_RETINA_FRAMEBUFFER COCOA_RETINA_FRAMEBUFFER</td><td>GLFW_TRUE TRUE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * <tr><td>GLFW_COCOA_GRAPHICS_SWITCHING COCOA_GRAPHICS_SWITCHING</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
-     * </table>
+	 * Windows hints for the next window created.
 	 */
 	public static class Hints
 	{
@@ -205,7 +180,117 @@ public class GLFWWindow extends GLFWHandle
 			GLFW.glfwWindowHint(GLFW.GLFW_STENCIL_BITS, value);
 		}
 		
+		/**
+		 * Sets the next created window's accumulation framebuffer's red color bits.
+		 * @param value 0 to {@link Integer#MAX_VALUE}, or -1 for "Don't Care".
+		 */
+		public static void setAccumRedBits(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_ACCUM_RED_BITS, value);
+		}
+		
+		/**
+		 * Sets the next created window's accumulation framebuffer's green color bits.
+		 * @param value 0 to {@link Integer#MAX_VALUE}, or -1 for "Don't Care".
+		 */
+		public static void setAccumGreenBits(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_ACCUM_GREEN_BITS, value);
+		}
+		
+		/**
+		 * Sets the next created window's accumulation framebuffer's blue color bits.
+		 * @param value 0 to {@link Integer#MAX_VALUE}, or -1 for "Don't Care".
+		 */
+		public static void setAccumBlueBits(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_ACCUM_BLUE_BITS, value);
+		}
+		
+		/**
+		 * Sets the next created window's accumulation framebuffer's alpha bits.
+		 * @param value 0 to {@link Integer#MAX_VALUE}, or -1 for "Don't Care".
+		 */
+		public static void setAccumAlphaBits(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_ACCUM_ALPHA_BITS, value);
+		}
+		
+		/**
+		 * Sets the next created window's auxiliary buffers.
+		 * @param value 0 to {@link Integer#MAX_VALUE}.
+		 */
+		public static void setAuxBuffers(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_AUX_BUFFERS, value);
+		}
+		
+		/**
+		 * Sets the next created window's multisample samples.
+		 * @param value 0 to {@link Integer#MAX_VALUE}.
+		 */
+		public static void setSamples(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_SAMPLES, value);
+		}
+		
+		/**
+		 * Sets the next created window's refresh rate.
+		 * @param value 0 to {@link Integer#MAX_VALUE}, or -1 for "Don't Care".
+		 */
+		public static void setRefreshRate(int value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_REFRESH_RATE, value);
+		}
+		
+		/**
+		 * Sets if the next created window is in stereo mode.
+		 * @param value true if so, false if not.
+		 */
+		public static void setRefreshRate(boolean value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_STEREO, value ? GLFW.GLFW_TRUE : GLFW.GLFW_FALSE);
+		}
+
+		/**
+		 * Sets if the next created window is SRGB Capable.
+		 * @param value true if so, false if not.
+		 */
+		public static void setSRGBCapable(boolean value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_SRGB_CAPABLE, value ? GLFW.GLFW_TRUE : GLFW.GLFW_FALSE);
+		}
+
+		/**
+		 * Sets if the next created window is double-buffered.
+		 * @param value true if so, false if not.
+		 */
+		public static void setDoubleBuffered(boolean value)
+		{
+			GLFW.glfwWindowHint(GLFW.GLFW_DOUBLEBUFFER, value ? GLFW.GLFW_TRUE : GLFW.GLFW_FALSE);
+		}
+
 		// TODO: Finish.
+		
+		/**
+		 * Window hints.
+	     * <table class=striped>
+	     * <tr><th>Name</th><th>Default value</th><th>Supported values</th></tr>
+	     * <tr><td>GLFW_CLIENT_API CLIENT_API</td><td>GLFW_OPENGL_API OPENGL_API</td><td>GLFW_NO_API NO_API GLFW_OPENGL_API OPENGL_API GLFW_OPENGL_ES_API OPENGL_ES_API</td></tr>
+	     * <tr><td>GLFW_CONTEXT_CREATION_API CONTEXT_CREATION_API</td><td>GLFW_NATIVE_CONTEXT_API NATIVE_CONTEXT_API</td><td>GLFW_NATIVE_CONTEXT_API NATIVE_CONTEXT_API GLFW_EGL_CONTEXT_API EGL_CONTEXT_API GLFW_OSMESA_CONTEXT_API OSMESA_CONTEXT_API</td></tr>
+	     * <tr><td>GLFW_CONTEXT_VERSION_MAJOR CONTEXT_VERSION_MAJOR</td><td>1</td><td>Any valid major version number of the chosen client API</td></tr>
+	     * <tr><td>GLFW_CONTEXT_VERSION_MINOR CONTEXT_VERSION_MINOR</td><td>0</td><td>Any valid minor version number of the chosen client API</td></tr>
+	     * <tr><td>GLFW_CONTEXT_ROBUSTNESS CONTEXT_ROBUSTNESS</td><td>GLFW_NO_ROBUSTNESS NO_ROBUSTNESS</td><td>GLFW_NO_ROBUSTNESS NO_ROBUSTNESS GLFW_NO_RESET_NOTIFICATION NO_RESET_NOTIFICATION GLFW_LOSE_CONTEXT_ON_RESET LOSE_CONTEXT_ON_RESET</td></tr>
+	     * <tr><td>GLFW_CONTEXT_RELEASE_BEHAVIOR CONTEXT_RELEASE_BEHAVIOR</td><td>GLFW_ANY_RELEASE_BEHAVIOR ANY_RELEASE_BEHAVIOR</td><td>GLFW_ANY_RELEASE_BEHAVIOR ANY_RELEASE_BEHAVIOR GLFW_RELEASE_BEHAVIOR_FLUSH RELEASE_BEHAVIOR_FLUSH GLFW_RELEASE_BEHAVIOR_NONE RELEASE_BEHAVIOR_NONE</td></tr>
+	     * <tr><td>GLFW_CONTEXT_NO_ERROR CONTEXT_NO_ERROR</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
+	     * <tr><td>GLFW_OPENGL_FORWARD_COMPAT OPENGL_FORWARD_COMPAT</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
+	     * <tr><td>GLFW_OPENGL_DEBUG_CONTEXT OPENGL_DEBUG_CONTEXT</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
+	     * <tr><td>GLFW_OPENGL_PROFILE OPENGL_PROFILE</td><td>GLFW_OPENGL_ANY_PROFILE OPENGL_ANY_PROFILE</td><td>GLFW_OPENGL_ANY_PROFILE OPENGL_ANY_PROFILE GLFW_OPENGL_CORE_PROFILE OPENGL_CORE_PROFILE GLFW_OPENGL_COMPAT_PROFILE OPENGL_COMPAT_PROFILE</td></tr>
+	     * <tr><td>GLFW_COCOA_RETINA_FRAMEBUFFER COCOA_RETINA_FRAMEBUFFER</td><td>GLFW_TRUE TRUE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
+	     * <tr><td>GLFW_COCOA_GRAPHICS_SWITCHING COCOA_GRAPHICS_SWITCHING</td><td>GLFW_FALSE FALSE</td><td>GLFW_TRUE TRUE or GLFW_FALSE FALSE</td></tr>
+	     * </table>
+		 */
+
 	}
 	
 	/**
